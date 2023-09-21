979acc193643abe71e9b7c3ee74a1aaf6257ddde
Tsatsu Cephas <tsatsuc@gmail.com>
2023-09-21 18:06:29 +0000

    auto close nav after click

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -1,7 +1,16 @@
 const hamburgerButton = document.getElementById("hamburger-button");
 const navbar = document.getElementById("navbar");
+const navLinks = document.querySelectorAll(".nav-link");
 
 hamburgerButton.addEventListener("click", () => {
     hamburgerButton.classList.toggle("active");
     navbar.classList.toggle("active");
+});
+
+// Close the hamburger menu when a link is clicked
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        hamburgerButton.classList.remove("active");
+        navbar.classList.remove("active");
+    });
 });
