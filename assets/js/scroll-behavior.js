79b5c654ce019aefc66b8e725519efd27081cb2e
Tsatsu Cephas <tsatsuc@gmail.com>
2023-09-22 01:49:04 +0000

    Added Resume Section

---
+++ assets/js/scroll-behavior.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("a[href^='#']");
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
